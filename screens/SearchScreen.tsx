584480e0cb24edc25df5731e003ed9aa0ff08064
Konstantinos Zafeiropoulos <k.a.zafeiropoulos@gmail.com>
2021-09-19 16:51:14 +0200

    Update SearchScreen

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,69 +1,14 @@
-
-import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableNativeFeedback } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList } from './RootStackPrams';
-import { FlatList } from 'react-native-gesture-handler';
-
-
-type searchScreenProp = StackNavigationProp<RootStackParamList, 'Search'>;
+import React from 'react';
+import { View } from 'react-native';
+import SearchContainer from "../components/SearchBarComponent";
+import ResultList from "../components/ResultListComponent";
 
 function SearchScreen() {
-    const navigation = useNavigation<searchScreenProp>();
-
-    const [filterdData, setfilterdData] = useState([]);
-    const [masterData, setmasterData] = useState([]);
-
-    useEffect(() => {
-        fetchRepos();
-        return()=>{
-
-        }
-    }, [])
-
-    const fetchRepos = () => {
-        const apiURL = 'https://jsonplaceholder.typicode.com/posts';
-        fetch(apiURL)
-        .then((response) => response.json() )
-        .then((responseJson) => {
-            setfilterdData(responseJson);
-            setmasterData(responseJson);
-        }).catch((error) => {
-            console.error(error);
-        })
-    }
-
-    const ItemView = ({item}:{item:any}) => {
-        return (
-            <TouchableNativeFeedback onPress={() => navigation.navigate('Details')}>
-                <Text style={{padding: 15}}>
-                    {item.id}{'. '}{item.title}
-                </Text>
-            </TouchableNativeFeedback>
-
-
-        )
-    }
-
-    const ItemSeparatorView = () => {
-        return(
-            <View 
-                style={{height: 0.5, width: '100%', backgroundColor: '#c8c8c8'}} 
-            />
-        )
-    }
-
     return (
         <View style={{backgroundColor: 'white'}}>
-            <FlatList
-                data={filterdData}
-                keyExtractor={(item, index) => index.toString()}
-                ItemSeparatorComponent={ItemSeparatorView}
-                renderItem={ItemView}
-            />
+            <SearchContainer />
+            <ResultList />
         </View>
-        
     );
 }
 
