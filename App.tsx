bd393378edb8b526820fef60da08dc4c4906359d
Konstantinos Zafeiropoulos <k.a.zafeiropoulos@gmail.com>
2021-09-19 16:32:45 +0200

    Installed graphql and apollo client for API calls

--- App.tsx
+++ App.tsx
@@ -9,16 +9,58 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { RootStackParamList } from './screens/RootStackPrams';
 
+import {createStore} from 'redux';
+import allReducers from './reducers/index';
+import { Provider, useDispatch } from 'react-redux';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const githubToken = ""; //TODO: Use your tokem
+
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
+
+  const  store = createStore(allReducers);
+
+  const httpLink = new HttpLink({
+    uri: "https://api.github.com/graphql"
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = "Bearer " + githubToken;
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? token : null
+      }
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+  });
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Search" component={SearchScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <Provider store = {store}>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Search" component={SearchScreen} />
+            <Stack.Screen name="Details" component={DetailsScreen} />
+          </Stack.Navigator>
+          <StatusBar style="auto" />
+        </NavigationContainer>
+      </ApolloProvider>
+    </Provider>
+    
   );
 }
